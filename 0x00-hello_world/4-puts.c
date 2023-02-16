60313416b7c3b1d88650860d5342406658955e53
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-16 07:17:03 -0800

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"/n)
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle")
+        return (0);
 }
