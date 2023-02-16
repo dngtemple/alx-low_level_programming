231151da43bd8037d45807b1cb37bd52517775f3
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-16 10:31:37 -0800

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,14 @@ int main(void)
 int a;
 float b;
 char c;
+long int d;
+long long e;
 
 printf("Size of an int :%lu byte(s)", (unsigned long)sizeof(a));
 printf("Size of a float :%lu byte(s)", (unsigned long)sizeof(b));
 printf("Size of a char:%lu byte(s)", (unsigned long)sizeof(c));
+printf("Size of a long int:%lu byte(s)", (unsigned long)sizeof(d));
+printf("Size of a long long:%lu byte(s)", (unsigned long)sizeof(e));
+
 return (0);
 }
