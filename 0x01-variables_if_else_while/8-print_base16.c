a4cf2d80b0e9afbdc66c3f4b7919b1cfcc4b8828
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-17 09:36:02 -0800

    base 16 lowercase letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,14 @@
  */
 int main(void)
 {
+int digit;
 char letter;
+
+for (digit = 0; digit < 10; digit++)
+{
+putchar('0' + digit);
+}
+
 for (letter = 'a'; letter <= 'f'; letter++)
 {
 putchar(letter);
