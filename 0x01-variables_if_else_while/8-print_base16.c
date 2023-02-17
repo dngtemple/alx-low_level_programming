6bd5aafa715727f5ec1ebc972c592bbd40baf41f
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-17 09:30:57 -0800

    base 16 lowercase letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+
+return (0);
+}
