db0fa335bcac8afe4872b3797e5169a42e36d660
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-21 12:21:42 -0800

    adding 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - printing the sum of two integers
+ * @a: first number to be added
+ * @b: second number to be added
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+int  result = a + b;
+return (result);
+}
