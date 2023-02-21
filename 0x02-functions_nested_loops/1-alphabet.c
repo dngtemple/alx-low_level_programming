57e0f42388d552f533db43ac658820d64a11e991
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-21 05:54:34 -0800

    displaying alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphhabets in lower case
+ */
+
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+
