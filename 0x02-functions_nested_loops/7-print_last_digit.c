a4128b92d51536b11eaadabf0796bb0d54f2fc7c
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-21 08:07:37 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "math.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: the number in question
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+int last_digit = n  % 10;
+
+if (last_digit < 0)
+{
+last_digit *= -1;
+}
+_putchar('0' + last_digit);
+
+return (last_digit);
+}
