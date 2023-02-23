5864a4125a2c199f22f716a0fb69c21f700e3214
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-23 08:41:51 -0800

    more nested task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checking if a character is uppercase
+ * @c: character to be checked
+ *
+ * Return: 0 if success
+ *         1 if failure
+ */
+
+
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
