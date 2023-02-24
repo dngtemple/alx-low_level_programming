916dd3f933f0c188fcfce9a7fb136fc0f9f4ccb5
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-23 16:37:07 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - printing fizz buzz
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 100; num++)
+{
+if (num % 3 == 0)
+{
+printf("Fizz");
+}
+if (num % 5 == 0)
+{
+printf("Buzz");
+}
+if  (num % 15 == 0)
+{
+printf("FizzBuzz");
+}
+}
+printf("\n");
+return (0);
+}
