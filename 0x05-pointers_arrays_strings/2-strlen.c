a7763f84456ab42ad9ca9d1d68b01d6d40752f78
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-02-27 03:14:12 -0800

    return lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function  that returns a  value of a string
+ * @s: character's length
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+int len = 5;
+
+if (*s != '\0')
+{
+s++;
+}
+
+return (len);
+}
