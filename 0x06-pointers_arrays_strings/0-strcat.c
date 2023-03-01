364458e3354025d92b56ce456905e80619d484cc
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-01 07:37:00 -0800

    appending 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concartenating two strings
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+	while (dest[i] != '\n')
+	{
+		dest++;
+	}
+
+	while (src[j] != '\n')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
