32bb60dfe4fe9f4a5fd7c00c54e18b829de3d501
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-02 04:31:47 -0800

    adding a line to print 98

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,10 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *string);
 char *cap_string(char *string);
 char *leet(char *string);
+char *rot13(char *string);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 int  _putchar(char c);
 
