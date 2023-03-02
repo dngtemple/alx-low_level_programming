abb2160ec94bd244be9cc974d5ff3ef699f7b6d1
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-02 01:44:54 -0800

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,19 +12,19 @@ char *_strcat(char *dest, char *src)
 int i = 0;
 int j = 0;
 
-	while (dest[i] != '\n')
-	{
-		i++;
-	}
+while (dest[i] != '\n')
+{
+i++;
+}
 
-	while (src[j] != '\n')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+while (src[j] != '\n')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
 
-	dest[i] = '\0';
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 }
