863a80d2b4aed40ccb64195a8eaa83ab57db659a
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-02 02:18:58 -0800

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,15 @@
 
 /**
  * _strcmp - comparing two strings
- * @s1: first string
- * @s2: second string
+ * @s1: a pointer that will be changed
+ * @s2: a pointer that will also be modified
  * Return: *string_1 - *string_2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-char *string_1;
-char *string_2;
+char *string_1 = s1;
+char *string_2 = s2;
 
 while (*string_1 != '\0' && *string_2 != '\0' && *string_1 == *string_2)
 {
