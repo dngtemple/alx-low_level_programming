15cca6e734968ab5e14bebd8f32eea297d0fe602
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-03 07:54:34 -0800

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,9 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *rot13(char *s)
+int isPrintableASCII(int n);
+void printHexes(char *b, int start, int end);
+void printASCII(char *b, int start, int end);
 
 int  _putchar(char c);
 
