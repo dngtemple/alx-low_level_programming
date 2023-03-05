68793b45e59e4e2e318bbb5e52af921ff32196b4
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-05 09:51:47 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - printing chess boaurd
+ * @a: char
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i;
+
+int b;
+
+for (i = 0; i <= 8; i++)
+{
+for (b = 0; b <= 8; b++)
+{
+_putchar(a[i][b]);
+}
+_putchar('\n');
+}
+}
