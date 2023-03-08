6043cf6de81b5d8d9330960586c61afe0dc34450
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-08 11:37:52 -0800

    returns lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ * @s: pointer to a string
+ * Return: 0
+ *         1
+ *         _strlen_recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
+}
+
+
