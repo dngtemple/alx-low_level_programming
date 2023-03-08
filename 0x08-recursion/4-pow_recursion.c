70e9e5050e3216143e6acd1029285b6df4ccb34e
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-08 12:16:29 -0800

    x pow y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return value of x to be power of y
+ * @x: value to be raised
+ * @y: value to raise to
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 1)
+{
+return (x);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
