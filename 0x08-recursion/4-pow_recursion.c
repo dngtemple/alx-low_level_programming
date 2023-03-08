bb99ffa8cd83f7c9613924531477f2ba6cd0c5f2
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-08 12:18:23 -0800

    x pow y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,9 @@
  * _pow_recursion - return value of x to be power of y
  * @x: value to be raised
  * @y: value to raise to
+ * Return: 0
+ *         1
+ *         _pow_recursion
  */
 
 int _pow_recursion(int x, int y)
