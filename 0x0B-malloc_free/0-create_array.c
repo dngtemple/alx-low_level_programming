aed6708c5210fced4ba4d25c45cd9e6d27051b4a
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-14 03:44:03 -0700

    creating an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chacracters
+ * @size: size oa array
+ * @c: characters
+ *
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *s;
+int i = 0;
+
+s = malloc(sizeof(char) * size);
+
+if (s == 0)
+{
+return (NULL);
+}
+
+while (i < size)
+{
+s[i] = c;
+i++;
+}
+return (s);
+}
