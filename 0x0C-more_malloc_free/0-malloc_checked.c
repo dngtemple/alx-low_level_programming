a22191aa9dd6292fdc18df079025e0957266cbc6
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-16 04:54:07 -0700

    allocating mem using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "'main.h"
+
+/**
+ * malloc_checked - allocating memory using malloc
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *j;
+
+j = malloc(b);
+
+if(j == NULL)
+{
+exit (98);
+}
+
+return (i);
+}
