655113ffb429ca28da119a83ef471d0b313a7ac9
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-20 12:54:03 -0700

    frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory  of dogs
+ * @d: pointer to dogs
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
