4fea6a8da59e1711821af546786eaf1ba710b274
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-03-23 07:29:59 -0700

    prints the sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all its parameters.
+ * @n: number of parameters
+ *
+ * Return: 0
+ *         sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUCNTIONS_H
+#include <stdarg.h>
+
 
 int _putchar(char c);
 
